7bd3c062218e4e68aeabd32cea63e151380334fa
nicosiahaan <57294961+nicosiahaan@users.noreply.github.com>
2021-06-10 14:58:24 +0700

    finishing draw maze and result .gif file

--- maze.py
+++ maze.py
@@ -1,3 +1,6 @@
+from PIL import Image, ImageDraw
+images = []
+
 maze = [
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
     [1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0 ,0, 0, 0, 0, 0, 0, 0, 0, 1],
@@ -34,6 +37,35 @@ def print_m(m):
         for j in range(len(m[i])):
             print( str(m[i][j]).ljust(2),end=' ')
         print()
+        
+def draw_matrix(a,m, the_path = []):
+    im = Image.new('RGB', (zoom * len(a[0]), zoom * len(a)), (255, 255, 255))
+    draw = ImageDraw.Draw(im)
+    for i in range(len(a)):
+        for j in range(len(a[i])):
+            color = (255, 255, 255)
+            r = 0
+            if a[i][j] == 1:
+                color = (0, 0, 0)
+            if i == starting_point[0] and j == starting_point[1]:
+                color = (0, 255, 0)
+                r = borders
+            if i == end_point[0] and j == end_point[1]:
+                color = (0, 255, 0)
+                r = borders
+            draw.rectangle((j*zoom+r, i*zoom+r, j*zoom+zoom-r-1, i*zoom+zoom-r-1), fill=color)
+            if m[i][j] > 0:
+                r = borders
+                draw.ellipse((j * zoom + r, i * zoom + r, j * zoom + zoom - r - 1, i * zoom + zoom - r - 1),
+                               fill=(255,0,0))
+    for u in range(len(the_path)-1):
+        y = the_path[u][0]*zoom + int(zoom/2)
+        x = the_path[u][1]*zoom + int(zoom/2)
+        y1 = the_path[u+1][0]*zoom + int(zoom/2)
+        x1 = the_path[u+1][1]*zoom + int(zoom/2)
+        draw.line((x,y,x1,y1), fill=(255, 0,0), width=5)
+    draw.rectangle((0, 0, zoom * len(a[0]), zoom * len(a)), outline=(0,255,0), width=2)
+    images.append(im)
 
 #Create Matrix and Set starting point
 matrix = []
@@ -77,3 +109,17 @@ while numOfStep > 1:
     the_path.append((i, j))
     numOfStep -= 1
   draw_matrix(maze, matrix, the_path)
+
+for i in range(10):
+    if i % 2 == 0:
+        draw_matrix(maze, matrix, the_path)
+    else:
+        draw_matrix(maze, matrix)
+
+print_m(matrix)
+print(the_path)
+
+# Save result as .gif file
+images[0].save('maze.gif',
+               save_all=True, append_images=images[1:],
+               optimize=False, duration=1, loop=0)
